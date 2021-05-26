c4758eaf29741cb690391cb207934349baa5c986
ujjawalgit <ujjawalkumarsingh08@gmail.com>
2021-05-26 10:31:14 +0530

    hello2

--- hello - Copy.java
+++ hello - Copy.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args){
-    System.out.println("First Java program - Hello world2");
+    System.out.println("First Java program - Hello world");
   }
 }
